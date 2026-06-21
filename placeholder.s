964958c1df869bddc2b56d3b20126b44e86189c6
Ivan Sorokin <unxed@mail.ru>
2026-06-21 12:44:58 +0200

    Fix build

---
+++ placeholder.s
@@ -0,0 +1,3 @@
+// This empty assembly file is a placeholder to trick the Go compiler.
+// It marks the package as "incomplete", which is required to allow
+// the use of //go:linkname when compiled as a dependency in external projects.

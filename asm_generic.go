edbe6375254612a59b2286a937f5fcbd38bb3e4c
Alexander Skakov <zoinen@gmail.com>
2026-08-22 12:15:49 +0300

    zlib4go: support generic hash chains on 32-bit targets

--- asm_generic.go
+++ asm_generic.go
@@ -5,8 +5,11 @@ package zlib_wasm
 import "unsafe"
 
 func updateHashChain(memBase unsafe.Pointer, pos, endPos, windowOffset, headOffset, prevOffset int32, masks uint64, shift, hash int32) int32 {
-	mem := (*[0x7fffffff]byte)(memBase)
-	mem16 := (*[0x3fffffff]uint16)(memBase)
+	// Use slices instead of giant array types: the latter are rejected by the
+	// compiler on 32-bit targets even though the backing window is addressed
+	// only at runtime.
+	mem := unsafe.Slice((*byte)(memBase), 0x7fffffff)
+	mem16 := unsafe.Slice((*uint16)(memBase), 0x3fffffff)
 	wMask := int32(masks)
 	hMask := int32(masks >> 32)
 	for pos < endPos {
